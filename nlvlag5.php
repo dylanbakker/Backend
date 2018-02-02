a4cec4738359b569a61544676c6ebf16c51c2afb
dylanbakker <36030671+dylanbakker@users.noreply.github.com>
2018-02-02 10:23:17 +0100

    Add files via upload

---
+++ nlvlag5.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <title>NLvlag</title>
+</head>
+<body>
+<style>
+<?php include 'nlvlag5.css'; ?>
+</style>
+<?php
+echo "<hr id='red'>";
+echo "<hr id='kleur1'>";
+echo "<hr id='kleur'>";
+?>
+</body>
+</html>
---
+++ weekdagen.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <title>weekdagen</title>
+</head>
+<body>
+<?php
+echo "<br>Maandag";
+echo "<br>Dinsdag";
+echo "<br>Woensdag";
+echo "<br>Donderdag";
+echo "<br>Vrijdag";
+echo "<br>Zaterdag";
+echo "<br>Zondag";
+?>
+</body>
+</html>
